f04f1349337e3c098ac7e3e500965c165fecf51d
Bartosz Adamczyk <contact@bartekadamczyk.eu>
2024-05-30 20:33:13 +0200

    tensorboard logging, accuracy rate and augmented classification works

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -it -v /run/media/adamczykb/HDD1/ai/machine-learning-theory:/app --ipc=host --gpus all -p 8888:8888 -p 6006:6006 mlt
+docker run -it -v $(pwd):/app --ipc=host --gpus all -p 8888:8888 -p 6006:6006 mlt
